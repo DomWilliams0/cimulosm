84bd0aa29ac0a3de1b8d80737421ed2667586d0f
Dom Williams <me@domwillia.ms>
2017-12-06 22:07:35 +0000

    Add OSM caching

--- src/world.rs
+++ src/world.rs
@@ -1,9 +1,11 @@
 use error::*;
-use std::io::Read;
+use std::io::{Read, Write};
 use reqwest;
 use parser;
+use std::{fs, env};
 
-const CHUNK_LAT: f64 = 0.0088; // y
+const CHUNK_LAT: f64 = 0.0088;
+// y
 const CHUNK_LON: f64 = 0.0144; // x
 
 #[derive(Clone)]
@@ -56,20 +58,39 @@ impl World {
     }
 }
 
-fn request_bbox(min_lat: f64, max_lat: f64, min_lon: f64, max_lon: f64)
-                -> SimResult<parser::World> {
-    let mut xml = String::new();
-    let mut resp = reqwest::get(
-        format!("http://overpass-api.de/api/map?bbox={},{},{},{}",
-                min_lon, min_lat, max_lon, max_lat).as_str())?;
+fn fetch_xml(min_lat: f64, max_lat: f64, min_lon: f64, max_lon: f64) -> SimResult<String> {
+    let cache = {
+        let mut p = env::temp_dir();
+        p.push(format!("chunk-cache--{}-{}-{}-{}.osm", min_lat, max_lat, min_lon, max_lon));
+        p
+    };
 
-    {
+    if cache.is_file() {
+        println!("Loading cached chunk");
+        let mut contents = String::new();
+        fs::File::open(cache)?.read_to_string(&mut contents)?;
+        Ok(contents)
+    } else {
+        println!("Requesting chunk");
+        let mut resp = reqwest::get(
+            format!("http://overpass-api.de/api/map?bbox={},{},{},{}",
+                    min_lon, min_lat, max_lon, max_lat).as_str())?;
+
         let status = resp.status();
         if !status.is_success() {
             return Err(ErrorKind::OsmRequest(resp.status().as_u16() as i32).into());
         }
-    }
 
-    resp.read_to_string(&mut xml)?;
-    parser::parse_osm(xml)
+        let mut xml = String::new();
+        resp.read_to_string(&mut xml)?;
+
+        fs::File::create(cache)?.write_all(xml.as_bytes())?;
+
+        Ok(xml)
+    }
+}
+
+fn request_bbox(min_lat: f64, max_lat: f64, min_lon: f64, max_lon: f64)
+                -> SimResult<parser::World> {
+    parser::parse_osm(fetch_xml(min_lat, max_lat, min_lon, max_lon)?)
 }
