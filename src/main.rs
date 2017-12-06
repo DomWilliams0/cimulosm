318ee865d3ac929b94e47b952b09b094cdd10c62
Dom Williams <me@domwillia.ms>
2017-12-06 11:22:35 +0000

    Add SFML render loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate sfml;
+
+use sfml::graphics::*;
+use sfml::window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut window = RenderWindow::new(
+        (300, 300),
+        "Hiya",
+        Style::NONE,
+        &Default::default()
+    );
+
+    window.set_framerate_limit(60);
+
+    let background_colour = Color::rgb(40, 40, 50);
+    loop {
+        while let Some(e) = window.poll_event() {
+            match e {
+                Event::KeyPressed { code, .. } => {
+                    if code == Key::Escape {
+                        return;
+                    }
+                }
+                _ => {}
+            }
+        }
+
+        window.clear(&background_colour);
+
+        window.display();
+    }
 }
